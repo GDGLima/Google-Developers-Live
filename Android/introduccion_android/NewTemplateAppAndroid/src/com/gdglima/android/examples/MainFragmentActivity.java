f617cca1609284b8cbd2ea7086ee2e9d94858578
Eduardo J.  Medina Alfaro <emedinaa@gmail.com>
2014-06-12 16:00:44 -0500

    android template projects

---
+++ Android/introduccion_android/NewTemplateAppAndroid/src/com/gdglima/android/examples/MainFragmentActivity.java
@@ -0,0 +1,14 @@
+package com.gdglima.android.examples;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class MainFragmentActivity extends FragmentActivity 
+{
+	@Override
+	protected void onCreate(Bundle arg0) {
+		// TODO Auto-generated method stub
+		super.onCreate(arg0);
+		setContentView(R.layout.fragment_activity_main);
+	}
+}
